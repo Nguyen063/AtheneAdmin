66ead3d00a0b359086171f29c47a1f42bef618dc
Nguyen063 <93272712+Nguyen063@users.noreply.github.com>
2022-05-21 10:24:28 +0700

    hehehe

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { UserManagementComponent } from './user-management/user-management.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    UserManagementComponent
   ],
   imports: [
     BrowserModule,
